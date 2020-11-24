442c6945a0ab5b9a1543f931222320f5b7515895
Rutmank <rutmank@yandex.ru>
2020-11-24 21:38:51 +0200

    Update

---
+++ Notepad/Json/InOutput.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notepad.Json
+{
+    class InOutput // для обработки данных используется Json, методы для считывания и сохранения данных
+    {
+
+    }
+}
