8f81df15a5441fd026631099f525cc22f34652cd
Rutmank <rutmank@yandex.ru>
2020-11-27 21:41:30 +0200

    Update

--- Notepad/Json/InOutput.cs
+++ Notepad/Json/InOutput.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Notepad.Models;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +10,21 @@ namespace Notepad.Json
 {
     class InOutput // для обработки данных используется Json, методы для считывания и сохранения данных
     {
+        private readonly string PATH;
 
+        public InOutput(string path) 
+        { 
+
+        }
+
+        public BindingList<DoModel> LoadData() // Считывание данных 
+        {
+            return null;
+        }
+
+        public void SaveData(BindingList<DoModel> _dataList) // Сохранение данных на жесткий диск
+        {
+            
+        }
     }
 }
